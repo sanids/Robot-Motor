6f417ba6c642c687c6c91aa2eca609384268e2c5
Sanid Singhal <sanid64@gmail.com>
2019-03-11 16:29:21 -0700

    final

---
+++ Motor.c
@@ -0,0 +1,204 @@
+//  square.c: Uses timer 2 interrupt to generate a square wave in pin
+//  P2.0 and a 75% duty cycle wave in pin P2.1
+//  Copyright (c) 2010-2018 Jesus Calvino-Fraga
+//  ~C51~
+#include <stdio.h>
+#include <stdlib.h>
+#include <EFM8LB1.h>
+
+// ~C51~  
+
+#define SYSCLK 72000000L
+#define BAUDRATE 115200L
+
+#define OUT0 P2_0
+#define OUT1 P2_1
+int flag;
+int alternate;
+int delaya;
+int time;
+int ran;
+int choice;
+int pwma;
+int pwmb;
+volatile unsigned char pwm_count=0;
+
+
+void Timer3us(unsigned char us)
+{
+	unsigned char i;               // usec counter
+	
+	// The input for Timer 3 is selected as SYSCLK by setting T3ML (bit 6) of CKCON0:
+	CKCON0|=0b_0100_0000;
+	
+	TMR3RL = (-(SYSCLK)/1000000L); // Set Timer3 to overflow in 1us.
+	TMR3 = TMR3RL;                 // Initialize Timer3 for first overflow
+	
+	TMR3CN0 = 0x04;                 // Sart Timer3 and clear overflow flag
+	for (i = 0; i < us; i++)       // Count <us> overflows
+	{
+		while (!(TMR3CN0 & 0x80));  // Wait for overflow
+		TMR3CN0 &= ~(0x80);         // Clear overflow indicator
+	}
+	TMR3CN0 = 0 ;                   // Stop Timer3 and clear overflow flag
+}
+
+void waitms (unsigned int ms)
+{
+	unsigned int j;
+	for(j=ms; j!=0; j--)
+	{
+		Timer3us(249);
+		Timer3us(249);
+		Timer3us(249);
+		Timer3us(250);
+	}
+}
+
+
+
+
+char _c51_external_startup (void)
+{
+	// Disable Watchdog with key sequence
+	SFRPAGE = 0x00;
+	WDTCN = 0xDE; //First key
+	WDTCN = 0xAD; //Second key
+  
+	VDM0CN=0x80;       // enable VDD monitor
+	RSTSRC=0x02|0x04;  // Enable reset on missing clock detector and VDD
+
+	#if (SYSCLK == 48000000L)	
+		SFRPAGE = 0x10;
+		PFE0CN  = 0x10; // SYSCLK < 50 MHz.
+		SFRPAGE = 0x00;
+	#elif (SYSCLK == 72000000L)
+		SFRPAGE = 0x10;
+		PFE0CN  = 0x20; // SYSCLK < 75 MHz.
+		SFRPAGE = 0x00;
+	#endif
+	
+	#if (SYSCLK == 12250000L)
+		CLKSEL = 0x10;
+		CLKSEL = 0x10;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 24500000L)
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 48000000L)	
+		// Before setting clock to 48 MHz, must transition to 24.5 MHz first
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+		CLKSEL = 0x07;
+		CLKSEL = 0x07;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 72000000L)
+		// Before setting clock to 72 MHz, must transition to 24.5 MHz first
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+		CLKSEL = 0x03;
+		CLKSEL = 0x03;
+		while ((CLKSEL & 0x80) == 0);
+	#else
+		#error SYSCLK must be either 12250000L, 24500000L, 48000000L, or 72000000L
+	#endif
+	
+	P0MDOUT |= 0x10; // Enable UART0 TX as push-pull output
+	XBR0     = 0x01; // Enable UART0 on P0.4(TX) and P0.5(RX)                     
+	XBR1     = 0X00;
+	XBR2     = 0x40; // Enable crossbar and weak pull-ups
+
+	// Configure Uart 0
+	#if (((SYSCLK/BAUDRATE)/(2L*12L))>0xFFL)
+		#error Timer 0 reload value is incorrect because (SYSCLK/BAUDRATE)/(2L*12L) > 0xFF
+	#endif
+	SCON0 = 0x10;
+	TH1 = 0x100-((SYSCLK/BAUDRATE)/(2L*12L));
+	TL1 = TH1;      // Init Timer1
+	TMOD &= ~0xf0;  // TMOD: timer 1 in 8-bit auto-reload
+	TMOD |=  0x20;                       
+	TR1 = 1; // START Timer1
+	TI = 1;  // Indicate TX0 ready
+
+	// Initialize timer 2 for periodic interrupts
+	TMR2CN0=0x00;   // Stop Timer2; Clear TF2;
+	CKCON0|=0b_0001_0000; // Timer 2 uses the system clock
+	TMR2RL=(0x10000L-(SYSCLK/10000L)); // Initialize reload value
+	TMR2=0xffff;   // Set to reload immediately
+	ET2=1;         // Enable Timer2 interrupts
+	TR2=1;         // Start Timer2 (TMR2CN is bit addressable)
+
+	EA=1; // Enable interrupts
+
+  	
+	return 0;
+}
+
+int getsn (char * buff, int len){
+int pwm;
+int j;
+char c;
+for(j=0; j<(len-1); j++){
+c=getchar();
+buff[j]= c;
+
+}
+buff[j]= 0;
+pwm = atoi(buff);
+return pwm;
+}
+
+
+void Timer2_ISR (void) interrupt 5
+{
+	TF2H = 0; // Clear Timer2 interrupt flag
+	pwm_count++;
+	if(pwm_count>100) pwm_count=0;
+	if (choice == 1 || flag == 0){
+    OUT1 = 0;
+	OUT0=pwm_count>pwma?0:1;}
+    else if (choice == 0 || flag == 1) {
+    OUT0 = 0;
+	OUT1=pwm_count>pwma?0:1; }
+    
+    
+      
+ waitms(0); }
+
+void main (void)
+{
+    char buff[4];
+    delaya = 0;
+    OUT0=0;
+    OUT1=0;
+	printf("\x1b[2J"); // Clear screen using ANSI escape sequence.
+	printf("Square wave generator for the EFM8LB1.\r\n"
+	       "Check pins P2.0 and P2.1 with the oscilloscope.\r\n");
+	printf("\nEnter your speed from 1-100\r\n");
+	pwma = getsn(buff, sizeof(buff));
+	printf("\nEnter direction (1 for CCW, 0 for CW or 2 for alternating or 3 for random \r\n");
+	choice = getsn(buff, sizeof(buff));
+	if (choice == 2 || choice == 3) {
+	printf("Please select the alternating time in seconds. ");
+    time = getsn(buff, sizeof(buff));
+    delaya = time*1000; 
+    }
+    printf("\n%i, %i", pwma , choice);
+	while(1)
+	{
+    if (choice == 2){
+    while(1){
+    flag = 0;
+    waitms(delaya);
+    flag = 1; 
+    waitms(delaya); }
+	}
+	 if (choice == 3){
+    while(1){
+    flag = rand() % 2;
+    waitms(delaya);
+	}}
+}}
